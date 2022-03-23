c8fe4cff747228551756c33aaf914ceb5ee70096
Marcel Goldstein <marcel.goldstein@outlook.com>
2022-03-23 22:22:36 +0100

    extract "LONG_PATH_PREFIX" as const

--- ImageChecker/Const/CommonConst.cs
+++ ImageChecker/Const/CommonConst.cs
@@ -2,6 +2,8 @@
 
 public class CommonConst
 {
+    public const string LONG_PATH_PREFIX = @"\\?\"; // long file path syntax. Its need to prefix this to the original path when exceeding the length limit.
+
     public const string PROJECT_PAGE_URL = "https://github.com/marcelgoldstein/ImageChecker";
 
     public const string TEMP_FILES_ROOT = "ImageChecker";
